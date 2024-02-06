05cbccded2af74d94b8f08296580f46863be781d
klyzecarize <71118473+klyzecarize@users.noreply.github.com>
2024-02-06 14:34:04 +0800

    aszxc

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production'
+
 const nextConfig = {
+    // basePath: ''
     output: 'export',
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
